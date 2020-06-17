4c6b2d36a9747d60014986ede50a9fd4b74669f9
hzy1754785321 <1754785321@qq.com>
2020-06-17 16:59:32 +0800

    。

---
+++ QuickReplyTools/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.keywordsText = new System.Windows.Forms.TextBox();
+            this.replyText = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.findNextBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // keywordsText
+            // 
+            this.keywordsText.Font = new System.Drawing.Font("宋体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.keywordsText.Location = new System.Drawing.Point(218, 136);
+            this.keywordsText.Multiline = true;
+            this.keywordsText.Name = "keywordsText";
+            this.keywordsText.Size = new System.Drawing.Size(359, 33);
+            this.keywordsText.TabIndex = 0;
+            // 
+            // replyText
+            // 
+            this.replyText.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.replyText.Location = new System.Drawing.Point(218, 274);
+            this.replyText.Multiline = true;
+            this.replyText.Name = "replyText";
+            this.replyText.Size = new System.Drawing.Size(359, 33);
+            this.replyText.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(178, 408);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(185, 44);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "查找回复";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(151, 144);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "输入:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(151, 281);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "输出:";
+            // 
+            // findNextBtn
+            // 
+            this.findNextBtn.Enabled = false;
+            this.findNextBtn.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.findNextBtn.Location = new System.Drawing.Point(469, 408);
+            this.findNextBtn.Name = "findNextBtn";
+            this.findNextBtn.Size = new System.Drawing.Size(185, 44);
+            this.findNextBtn.TabIndex = 5;
+            this.findNextBtn.Text = "查找下一个";
+            this.findNextBtn.UseVisualStyleBackColor = true;
+            this.findNextBtn.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(797, 540);
+            this.Controls.Add(this.findNextBtn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.replyText);
+            this.Controls.Add(this.keywordsText);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "快捷回复工具";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox keywordsText;
+        private System.Windows.Forms.TextBox replyText;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button findNextBtn;
+    }
+}
+
---
+++ QuickReplyTools/Form1.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitExcelSetting();
+        }
+
+        Dictionary<string, string> replyDic = new Dictionary<string, string>();
+
+        List<String> blackList = new List<string>();
+
+        public string lastKeyWords;
+        private void InitExcelSetting()
+        {
+            var tableName = "Sheet1";
+            var fileName = "数据表.xlsx";
+            var filePath = System.IO.Directory.GetCurrentDirectory() + "/" + fileName;
+            var dataTbl = InputFromExcel(filePath, tableName);
+            for (int i = 0; i < dataTbl.Rows.Count; i++)
+            {
+                replyDic.Add(dataTbl.Rows[i][0].ToString(), dataTbl.Rows[i][1].ToString());
+            }
+        }
+
+        #region 获取Excel数据表
+        /// <summary>
+        /// 获取Excel文件数据表列表
+        /// </summary>
+        private static ArrayList GetExcelTables(string ExcelFileName)
+        {
+            DataTable dt = new DataTable();
+            ArrayList TablesList = new ArrayList();
+            if (File.Exists(ExcelFileName))
+            {
+                if (Path.GetExtension(ExcelFileName) == ".xlsx")
+                {
+                    using (OleDbConnection conn = new OleDbConnection("Provider = Microsoft.ACE.OLEDB.12.0;;Data Source=" + ExcelFileName + ";Extended Properties='Excel 12.0;HDR=Yes;IMEX=1';"))
+                    {
+                        try
+                        {
+                            conn.Open();
+                            dt = conn.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, new object[] { null, null, null, "TABLE" });
+                        }
+                        catch (Exception exp)
+                        {
+                            throw exp;
+                        }
+                        int tablecount = dt.Rows.Count;
+                        for (int i = 0; i < tablecount; i++)
+                        {
+                            string tablename = dt.Rows[i][2].ToString().Trim().TrimEnd('$');
+                            if (TablesList.IndexOf(tablename) < 0)
+                            {
+                                TablesList.Add(tablename);
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    using (OleDbConnection conn = new OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Extended Properties=Excel 8.0;Data Source=" + ExcelFileName))
+                    {
+                        try
+                        {
+                            conn.Open();
+                            dt = conn.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, new object[] { null, null, null, "TABLE" });
+                        }
+                        catch (Exception exp)
+                        {
+                            throw exp;
+                        }
+
+                        int tablecount = dt.Rows.Count;
+                        for (int i = 0; i < tablecount; i++)
+                        {
+                            string tablename = dt.Rows[i][2].ToString().Trim().TrimEnd('$');
+                            if (TablesList.IndexOf(tablename) < 0)
+                            {
+                                TablesList.Add(tablename);
+                            }
+                        }
+                    }
+                }
+            }
+            return TablesList;
+        }
+        #endregion 获取Excel数据表
+
+        #region Excel导出
+        /// <summary>
+        /// 将Excel文件导出至DataTable(第一行作为表头)
+        /// </summary>
+        /// <param name="ExcelFilePath">Excel文件路径</param>
+        /// <param name="TableName">数据表名，如果数据表名错误，默认为第一个数据表名</param>
+        private static DataTable InputFromExcel(string ExcelFilePath, string TableName)
+        {
+            ArrayList TableList = new ArrayList();
+            TableList = GetExcelTables(ExcelFilePath);
+            if (TableName.IndexOf(TableName) < 0)
+            {
+                TableName = TableList[0].ToString().Trim();
+            }
+
+            DataTable table = new DataTable();
+            OleDbConnection dbcon;
+            if (Path.GetExtension(ExcelFilePath) == ".xlsx")
+            {
+                dbcon = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + ExcelFilePath + "; Extended Properties = 'Excel 12.0;IMEX=1;HDR=Yes;ImportMixedTypes=Text'");
+            }
+            else
+            {
+                dbcon = new OleDbConnection(@"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + ExcelFilePath + ";Extended Properties=Excel 8.0;");
+            }
+            OleDbCommand cmd = new OleDbCommand("select * from [" + TableName + "$]", dbcon);
+            OleDbDataAdapter adapter = new OleDbDataAdapter(cmd);
+            try
+            {
+                if (dbcon.State == ConnectionState.Closed)
+                {
+                    dbcon.Open();
+                }
+                adapter.Fill(table);
+            }
+            catch (Exception exp)
+            {
+                throw exp;
+            }
+            finally
+            {
+                if (dbcon.State == ConnectionState.Open)
+                {
+                    dbcon.Close();
+                }
+            }
+            return table;
+        }
+        #endregion Excel导出
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(keywordsText.Text))
+            {
+                MessageBox.Show("请先输入要查找的内容!");
+                return;
+            }
+            blackList.Clear();
+            findNextBtn.Enabled = true;
+            foreach (var data in replyDic)
+            {
+                if (keywordsText.Text.Contains(data.Key))
+                {
+                    replyText.Text = data.Value;
+                    Clipboard.SetDataObject(data.Value, true);
+                    lastKeyWords = data.Key;
+                    findNextBtn.Visible = true;
+                    return;
+                }
+            }
+            MessageBox.Show("没有找到对应的回复");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            blackList.Add(lastKeyWords);
+            bool isBlackWord = false;
+            foreach (var data in replyDic)
+            {
+                if (keywordsText.Text.Contains(data.Key))
+                {
+                    foreach (var word in blackList)
+                    {
+                        if (data.Key == word)
+                        {
+                            isBlackWord = true;
+                            break;
+                        }
+                    }
+                    if (isBlackWord)
+                    {
+                        isBlackWord = false;
+                        continue;
+                    }
+                    replyText.Text = data.Value;
+                    Clipboard.SetDataObject(data.Value, true);
+                    lastKeyWords = data.Key;
+                   // findNextBtn.Visible = true;
+                    return;
+                }
+            }
+            MessageBox.Show("没有找到对应的回复");
+        }
+    }
+}
